cb8da7325fbbe2b3d737dd8eb4336aa3880f36bd
Hendri Jonathan <hendri.jonathan@binus.edu>
2025-06-05 02:48:35 +0700

    test

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const app = express();
 
 app.use(cors(
   {
-<<<<<<< HEAD
     origin: ['http://localhost:3000', 'https://trading-bot-fe-three.vercel.app'],
-=======
-    origin: ['http://localhost:3000', 'https://trading-bot-fe-three.vercel.app/'],
->>>>>>> 2adea1a0b1c80137a66236efe76f641b57a6fbee
     credentials: true
   }
 ));
